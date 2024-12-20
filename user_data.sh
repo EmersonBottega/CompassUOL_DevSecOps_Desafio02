ae6eacf481898b467b55ec50b3788ddcf6e0af89
EmersonBottega <emerbtts@hotmail.com>
2024-12-20 15:49:33 -0300

    feat: healthcheck para wordpress

--- user_data.sh
+++ user_data.sh
@@ -9,7 +9,7 @@ systemctl start docker
 systemctl enable docker
 
 mkdir -p /mnt/efs
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport <nomeDnsSeuEfs:/ /mnt/efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport <nomeDnsSeuEfs>:/ /mnt/efs
 chown ec2-user:ec2-user /mnt/efs
 
 mkdir -p /usr/lib/docker/cli-plugins
